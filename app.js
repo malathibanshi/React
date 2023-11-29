79bb6ef2e306abf6c1961aeb2e45e4db96488622
malathibanshi <87061416+malathibanshi@users.noreply.github.com>
2023-11-29 19:07:18 +0530

    Update2 app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
 var a =document.createElement("h1")
 a.innerText="hello world using java"
 var b = document.getElementById("root")
 b.appendChild(a)
+ReactDOM.render(
+  <div>
+  <h1>hello</h1>
+    <p>This is react paragraph</p>
+  </div>, 
+  document.getElementById("root")
+    );
+
 
