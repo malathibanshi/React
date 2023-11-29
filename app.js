08b2b98bf0ed9d8e05e63704aad38ee3013203ef
malathibanshi <87061416+malathibanshi@users.noreply.github.com>
2023-11-29 20:27:34 +0530

    Update24 app.js

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ a.innerText="hello world using java"
 var b = document.getElementById("root")
 b.appendChild(a)
 ReactDOM.render(
-  <div>
-  <h1>hello</h1>
-    <p>This is react paragraph</p>
-  </div>, 
-  document.getElementById("root")
+   <h1>heading</h1>
+  <ul class="anime">
+     <li>death note</li>
+     <li>demon slayer</li>
+     <li>naruto</li>
+  </ul>,
+   document.getElementById("root")
     );
 
 
