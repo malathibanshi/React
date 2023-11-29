30dea829ea499f5533ca05672e397dba208d8dff
malathi <malathibanshi20@gmail.com>
2023-11-29 15:18:38 +0530

    update

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,6 @@
+var a = document.createElement("h1");
+a.innerText = "hello world using java";
+var b = document.getElementById("root");
+b.appendChild(a);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
